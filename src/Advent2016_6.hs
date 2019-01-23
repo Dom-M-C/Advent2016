17c3be95cc98ca8f9a9c7fb59831a47abaeb4bda
Dominic Copeland <dominic.m.copeland@gmail.com>
2019-01-23 14:18:53 +0000

    made some inroad into solving part one with a Map

--- src/Advent2016_6.hs
+++ src/Advent2016_6.hs
@@ -1,1 +1,38 @@
 module Advent2016_6 where 
+
+import qualified Data.Map as Map
+import Data.List
+
+type ColumnIndex = Int
+type ColumnString = String
+
+data ColumnStrings = ColumnStrings (Map.Map ColumnIndex ColumnString)
+
+buildColumnString :: Char -> Maybe ColumnString -> ColumnString
+buildColumnString c Nothing = [c]
+buildColumnString c (Just cs) = c:cs
+
+buildRowMap :: String -> ColumnIndex -> Map.Map ColumnIndex ColumnString -> Map.Map ColumnIndex ColumnString 
+buildRowMap [] _ m = m
+buildRowMap (c:cs) idx m = 
+    let
+        columnString = buildColumnString c $ Map.lookup idx m
+        updatedMap = (Map.insert idx columnString m)
+        nextIndex = idx + 1
+    in
+        buildRowMap cs nextIndex updatedMap
+        
+buildColumnMap :: [String] -> Map.Map ColumnIndex ColumnString -> ColumnStrings 
+buildColumnMap [] m = ColumnStrings m
+buildColumnMap (row:rows) m = buildColumnMap rows (buildRowMap row 0 Map.empty)
+
+showColumnStrings :: ColumnStrings -> String
+showColumnStrings (ColumnStrings cmap)
+    | cmap == Map.empty = "<empty>"
+    | otherwise = mconcat . map columnPairToString $ Map.toList cmap
+
+columnPairToString :: (ColumnIndex, ColumnString) -> String
+columnPairToString (x, y) = "Position " <> (show x) <> ": " <> y <> "\n"
+
+instance Show ColumnStrings where
+    show = showColumnStrings           
