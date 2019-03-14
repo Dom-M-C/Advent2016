7eff5395c7572b5fdd2d667f88248cf2e3cd0411
Dominic Copeland <dominic.copeland@oakbrook.local>
2019-03-14 13:05:34 +0000

    re-ordered some functions, finished the rotate column implementation by updating the Position keys and unioning with the original Map

--- src/Advent2016_8.hs
+++ src/Advent2016_8.hs
@@ -3,24 +3,80 @@ module Advent2016_8 where
 import Data.List
 import qualified Data.Map as Map
 
-type Position = (Int, Int)
+type RowInt = Int
+type ColumnInt = Int
+type Position = (RowInt, ColumnInt)
     
 data PixelState = On | Off
 
 instance Show PixelState where
-    show = renderPixel
+    show Off = "."
+    show On = "#"
 
 type Pixels = Map.Map Position PixelState
 
-switchState Off = On
-switchState On = Off
+rowLength :: RowInt
+rowLength = 50
 
-turnOn :: PixelState -> PixelState
-turnOn a = On
+columnHeight :: ColumnInt
+columnHeight = 6
 
-renderPixel :: PixelState -> String
-renderPixel Off = "."
-renderPixel On = "#"
+pixelMap :: Pixels
+pixelMap = Map.fromList . map (\x -> (x, Off)) $ cartesianprod
+    where
+        cartesianprod = pure (,) <*> [0..rowLength-1] <*> [0..columnHeight-1]
+
+pixelColumns' :: ColumnInt -> Pixels -> [Pixels]
+pixelColumns' columnSize pix 
+    | length pix == 0 = []
+    | length pix < columnSize = [pix]
+    | otherwise = row : pixelColumns' columnSize rest
+    where
+        (row, rest) = Map.splitAt (columnSize) pix
+
+pixelColumnsToRows :: [Pixels] -> [Pixels]
+pixelColumnsToRows columns = (map Map.fromList . transpose . map Map.toList) columns
+
+pixelRows' :: ColumnInt -> Pixels -> [Pixels]
+pixelRows' columnSize = pixelColumnsToRows . pixelColumns' columnSize
+
+pixelRows :: Pixels -> [Pixels]
+pixelRows = pixelRows' columnHeight
+
+pixelColumns :: Pixels -> [Pixels]
+pixelColumns = pixelColumns' rowLength --do not trust this
+
+rect :: RowInt -> ColumnInt -> Pixels -> Pixels
+rect 0 _ pix = pix
+rect _ 0 pix = pix
+rect a b pix = 
+    let
+        onCoords = pure (,) <*> [0..a-1] <*> [0..b-1]
+        doUpdate [] screen = screen
+        doUpdate (hed:xs) screen = doUpdate xs $ Map.adjust (\_ -> On) hed screen
+    in
+        doUpdate onCoords pix
+
+rotateColumn :: RowInt -> Int -> Pixels -> Pixels
+rotateColumn columnRowPos n pix =
+    let
+        newPos y = (y + n) `mod` columnHeight
+        movePos ((x, y), state) = ((x, newPos y), state)
+        columnVals = movePos <$> (Map.toList . getAllColumnCells columnRowPos $ pix)
+    in
+        Map.union (Map.fromList columnVals) pix
+
+
+getAllColumnCells :: RowInt -> Pixels -> Pixels
+getAllColumnCells columnRowPos pix = Map.filterWithKey filterFunc pix
+    where
+        filterFunc (x, _) _ = x == columnRowPos
+        
+
+
+applyRects = rect 7 1 . rect 4 4 . rect 3 2 
+
+
 
 renderRows :: Pixels -> [String]
 renderRows = map (mconcat . map show) . map Map.elems . pixelRows
@@ -31,57 +87,5 @@ renderScreen = unlines . renderRows
 printScreen :: Pixels -> IO ()
 printScreen = putStrLn . renderScreen 
 
-rowLength :: Int
-rowLength = 50
-
-columnHeight :: Int
-columnHeight = 6
-
-cellCount = rowLength * columnHeight
-
-pixelMap :: Pixels
-pixelMap = Map.fromList . map (\x -> (x, Off)) $ cartesianprod
-    where
-        cartesianprod = pure (,) <*> [0..rowLength] <*> [0..columnHeight]
-        restricted = take cellCount cartesianprod
-
-pixelColumns' :: Int -> Pixels -> [Pixels]
-pixelColumns' columnSize pix 
-    | length pix == 0 = []
-    | length pix < columnSize = [pix]
-    | otherwise = row : pixelColumns' columnSize rest
-    where
-        (row, rest) = Map.splitAt (columnSize+1) pix
-
-pixelColumnsToRows :: [Pixels] -> [Pixels]
-pixelColumnsToRows columns = (map Map.fromList . transpose . map Map.toList) columns
-
-pixelRows' :: Int -> Pixels -> [Pixels]
-pixelRows' columnSize = pixelColumnsToRows . pixelColumns' columnSize
-
-pixelRows :: Pixels -> [Pixels]
-pixelRows = pixelRows' columnHeight
-
---pixelColumns :: Pixels -> [Pixels]
---pixelColumns = pixelColumns' rowLength --do not trust this
-
-rect :: Pixels -> Int -> Int -> Pixels
-rect pix 0 _ = pix
-rect pix _ 0 = pix
-rect pix a b = 
-    let
-        onCoords = pure (,) <*> [0..a-1] <*> [0..b-1]
-        doUpdate [] screen = screen
-        doUpdate (hed:xs) screen = doUpdate xs $ Map.adjust (\_ -> On) hed screen
-    in
-        doUpdate onCoords pix
-        
 
 
-{-
-
-
-rect :: [Pixel] -> Int -> Int -> [Pixel]
-rect p x y = undefined    
--}
---rotateRow :: Pixel -> Int -> 
