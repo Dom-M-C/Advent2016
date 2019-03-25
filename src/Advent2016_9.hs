4d94a870332d7fe0f2e43a179992b13e949520cf
Dominic Copeland <dominic.m.copeland@gmail.com>
2019-03-25 14:09:53 +0000

    implementations for parsing markers and extracting them and their associated text

--- src/Advent2016_9.hs
+++ src/Advent2016_9.hs
@@ -2,35 +2,46 @@
 
 module Advent2016_9 where
 
-
-
 import qualified Data.Text as T
 
 type DataLength = Int
 type RepetitionTimes = Int
 
-data Marker = Marker DataLength RepetitionTimes deriving Show
+data Marker = Marker
+    {   dataLength :: DataLength
+    ,   repetitionTimes :: RepetitionTimes
+    } deriving Show
 
 type CompressedText = T.Text
 type DecompressedString = String
+type PreprocessedText = (Marker, CompressedText)
 
+readText :: Read a => T.Text -> a
+readText = read . T.unpack
 
+parseMarker :: T.Text -> Marker
+parseMarker txt = Marker (readText dLength :: DataLength) (readText rTimes :: RepetitionTimes)
+    where
+        (dLength, rTimes) = (\(x, y) -> (x, T.drop 1 $ y) ) . T.breakOn "x" . T.drop 1 $ txt
 
-processMarker :: Marker -> CompressedText -> (DecompressedString, CompressedText)
-processMarker = undefined
-
-parseInput :: T.Text -> [(Marker, CompressedText)]
+parseInput :: T.Text -> [PreprocessedText]
+parseInput "" = []
 parseInput txt = 
     let
-        (mark, rest) = T.breakOn  ")" txt
-        (dLength, rTimes) = (\(x, y) -> (T.unpack x, T.unpack (T.drop 1 y)) ) . T.breakOn "x" . T.drop 1 $ mark
-        marker = Marker (read dLength :: DataLength) (read rTimes :: RepetitionTimes)
+        (mark, rest) = (\(x,y) -> (x, T.drop 1 y)) . T.breakOn  ")" $ txt
+        marker = parseMarker mark
+        next m = T.drop (dataLength m) rest
+        this m = T.take (dataLength m) rest
     in
-        [(marker, "")]
+        (marker, this marker) : parseInput (next marker)
 
+expandMarks :: PreprocessedText -> DecompressedString
+expandMarks = undefined
 
-parseMarker :: T.Text -> (T.Text)
-parseMarker = undefined
+processText :: T.Text -> [DecompressedString]
+processText = map expandMarks . parseInput 
 
 testInput :: T.Text
 testInput = "(6x9)JUORKH(10x13)LNWIKDMACM(126x14)(21x8)QLKUJNVVZIQGGFCJZMPHK(2x1)ZH"
+
+
