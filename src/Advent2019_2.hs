30f7ae61606e910739e73e627ca3628b7fdc1a72
Dominic Copeland <dmcop@windesk.dmc>
2020-05-21 22:24:26 +0100

    editor config

--- src/Advent2019_2.hs
+++ src/Advent2019_2.hs
@@ -4,56 +4,84 @@ module Advent2019_2 where
 
 import qualified Data.Text.IO as TIO
 import qualified Data.Text as T
+import Data.Map (Map)
 import qualified Data.Map as Map
-
+import Data.Vector (Vector, (!))
+import qualified Data.Vector as V
 
 input = do
     opcodeText <- TIO.readFile "src\\2019_2_input.txt"
     let opcodes = T.splitOn "," opcodeText
-    return $ map (\x -> (read . T.unpack) x :: Int) opcodes
+    let intcodes = map (\x -> (read . T.unpack) x :: Int) opcodes
+    return $ updateInput intcodes
 
 
 inputTuples = zip [0..] <$> input
 
 inputMap = Map.fromList <$> inputTuples
 
+inputVector = V.fromList <$> input
+
+type Address = Int
+
 data Opcode = Terminate
     | AddCode
-    {   paramIndexes :: (Int, Int)
-    ,   outputIndex :: Int
+    {   paramAddresses :: (Address, Address)
+    ,   outputAddress :: Address
     }
     | MultiplyCode
-    {   paramIndexes :: (Int, Int)
-    ,   outputIndex :: Int
-    } deriving Show
+    {   paramAddresses :: (Address, Address)
+    ,   outputAddress :: Address
+    } deriving Show 
 
 traverseInput (99:_) = Terminate:[]
 traverseInput (1:i1:i2:out:rest) = AddCode (i1, i2) out : traverseInput rest
 traverseInput (2:i1:i2:out:rest) = MultiplyCode (i1, i2) out : traverseInput rest
 
-doOpcode :: Num a => Opcode -> Map.Map Int a -> Map.Map Int a
+doOpcode :: Num a => Opcode -> Map Address a -> Map Address a
 doOpcode Terminate map = map
 doOpcode op map = setOutput op (doOperation op map) map
 
-getFirstArg :: Map.Map Int a -> Opcode -> a
-getFirstArg map = (Map.!) map . fst . paramIndexes
+getFirstArg :: Map Address a -> Opcode -> a
+getFirstArg map = (Map.!) map . fst . paramAddresses
 
-getSecondArg :: Map.Map Int a -> Opcode -> a
-getSecondArg map = (Map.!) map . snd . paramIndexes
+getSecondArg :: Map Address a -> Opcode -> a
+getSecondArg map = (Map.!) map . snd . paramAddresses
 
-setOutput :: Opcode -> a -> Map.Map Int a -> Map.Map Int a
-setOutput = Map.insert . outputIndex
+setOutput :: Opcode -> a -> Map Address a -> Map Address a
+setOutput = Map.insert . outputAddress
 
-doOperation :: Num a => Opcode -> Map.Map Int a -> a
+doOperation :: Num a => Opcode -> Map Address a -> a
 doOperation op map = (operation op) (getFirstArg map op) (getSecondArg map op)
     where
         operation (AddCode _ _) = (+)
         operation (MultiplyCode _ _) = (*)
 
-doOpcodes :: Num a => Map.Map Int a -> [Opcode] -> Map.Map Int a
+doOpcodes :: Num a => Map Address a -> [Opcode] -> Map Address a
 doOpcodes map (op:codes) = doOpcodes (doOpcode op map) codes
 doOpcodes map [] = map
 
+updateInput (x:_:_:rest) = x:12:2:rest
+
 partOne = doOpcodes <$> inputMap <*> (traverseInput <$> input)
 partTwo = undefined <$> input
- 
+
+
+
+type Memory = Vector Instruction
+
+data InstructionParameter = BinaryInstruction
+    {   inputParameters :: (Address, Address)
+    ,   outputParameter :: Address
+    }
+
+data Instruction = TerminateInstruction
+    | AddInstruction InstructionParameter
+    | MultiplyInstruction InstructionParameter
+
+data IntcodeProgram = IntcodeProgram
+    {   instructionPointer :: Instruction
+    ,   programMemory :: Memory
+    }
+
+initializeProgram vec = IntcodeProgram (vec ! 0) vec
